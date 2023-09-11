e390e85ff163f489363c88f576b941ccb94f0cc2
VADLURIVIDHATH <111587094+VADLURIVIDHATH@users.noreply.github.com>
2023-09-11 20:52:49 +0530

    Add files via upload

---
+++ Front.js
@@ -0,0 +1,14 @@
+import React , {Component}from 'react';
+import {Text,View,StyleSheet,Image,TouchableOpacity,TextInput}from 'react-native';
+import {Calender}from 'react-calender';
+import{Card}from 'react-native-paper';
+import{RFValue}from 'react-native-responsive-fontsize';
+
+export default class front extends Component{
+    constructor(){
+        super()
+        this.state= {
+            name:''
+        }
+    }
+}
---
+++ app.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import CreateTask from './CreateTask';
+import Calender from './Calender';
+import DisplayTask from './DisplayTask';
+import Front from './Front';
+import Home from './Home';
+import Intro from './Intro';
+import Task from './Task';
+
+const AppSwitchNavigator = createSwitchNavigator({
+    
+})
